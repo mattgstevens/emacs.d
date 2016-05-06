abd2e3c99190abb2596fa79a3818309c91041ec1
Martin Josefsson <hello@martinjosefsson.com>
2016-05-06 23:48:47 +0100

    Move long package configs into directory

---
+++ package-config/helm.el
@@ -0,0 +1,13 @@
+;; Helm all the things
+(require 'helm)
+(require 'helm-config)
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(require 'helm-projectile)
+(helm-projectile-on)
