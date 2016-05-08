e7867a58467a4e5b39a7d73d5791fead14b3db3d
Martin Josefsson <hello@martinjosefsson.com>
2016-05-08 15:18:15 +0100

    Add company-tern

--- package-config/company.el
+++ package-config/company.el
@@ -1,7 +1,12 @@
 ;; You're in good company
-(add-hook 'after-init-hook 'global-company-mode)
 (use-package company
   :ensure t
+  :ensure company-tern
   :config (progn (setq company-idle-delay 0)
+		 (setq company-tooltip-align-annotations t)
 		 (define-key company-active-map (kbd "TAB") 'company-select-next)
-		 (define-key company-active-map [tab] 'company-select-next)))
+		 (define-key company-active-map [tab] 'company-select-next)
+
+		 (add-to-list 'company-backends 'company-tern)))
+
+(add-hook 'after-init-hook 'global-company-mode)
