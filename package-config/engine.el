49551317a6627204999f391030817e4ef73f4bd0
Martin Josefsson <hello@martinjosefsson.com>
2016-08-07 13:22:06 +0900

    Purge port 80

--- package-config/engine.el
+++ package-config/engine.el
@@ -4,37 +4,37 @@
   :config (progn
 	    (engine-mode)
 	    (defengine amazon
-	      "http://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
+	      "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=%s")
 	    (defengine duckduckgo
-	      "https://duckduckgo.com/?q=%s"
+	      "httpss://duckduckgo.com/?q=%s"
 	      :keybinding "d")
 	    (defengine github
-	      "https://github.com/search?ref=simplesearch&q=%s")
+	      "httpss://github.com/search?ref=simplesearch&q=%s")
 	    (defengine google
-	      "http://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
+	      "https://www.google.com/search?ie=utf-8&oe=utf-8&q=%s"
 	      :keybinding "g")
 	    (defengine google-images
-	      "http://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
+	      "https://www.google.com/images?hl=en&source=hp&biw=1440&bih=795&gbv=2&aq=f&aqi=&aql=&oq=&q=%s")
 	    (defengine google-maps
-	      "http://maps.google.com/maps?q=%s"
+	      "https://maps.google.com/maps?q=%s"
 	      :docstring "Mappin' it up.")
 	    (defengine project-gutenberg
-	      "http://www.gutenberg.org/ebooks/search/?query=%s")
+	      "https://www.gutenberg.org/ebooks/search/?query=%s")
 	    (defengine rfcs
-	      "http://pretty-rfc.herokuapp.com/search?q=%s")
+	      "https://pretty-rfc.herokuapp.com/search?q=%s")
 	    (defengine stack-overflow
-	      "https://stackoverflow.com/search?q=%s")
+	      "httpss://stackoverflow.com/search?q=%s")
 	    (defengine twitter
-	      "https://twitter.com/search?q=%s")
+	      "httpss://twitter.com/search?q=%s")
 	    (defengine wikipedia
-	      "http://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
+	      "https://www.wikipedia.org/search-redirect.php?language=en&go=Go&search=%s"
 	      :keybinding "w"
 	      :docstring "Searchin' the wikis.")
 	    (defengine wiktionary
-	      "https://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
+	      "httpss://www.wikipedia.org/search-redirect.php?family=wiktionary&language=en&go=Go&search=%s")
 	    (defengine wolfram-alpha
-	      "http://www.wolframalpha.com/input/?i=%s")
+	      "https://www.wolframalpha.com/input/?i=%s")
 	    (defengine youtube
-	      "http://www.youtube.com/results?aq=f&oq=&search_query=%s")
+	      "https://www.youtube.com/results?aq=f&oq=&search_query=%s")
 
 	    (engine/set-keymap-prefix (kbd "C-c /"))))
