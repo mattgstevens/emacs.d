0dae250d2d4fed751d371d2bbcc2535591dbafcd
Martin Josefsson <hello@martinjosefsson.com>
2016-08-07 12:33:31 +0900

    Remove company-emoji as it's suuuuper slow and wants to use the : key

--- package-config/company.el
+++ package-config/company.el
@@ -1,8 +1,6 @@
 ;; You're in good company
-(add-hook 'after-init-hook 'global-company-mode)
 (use-package company
   :ensure t
-  :ensure company-emoji
   :config (progn (setq company-idle-delay 0)
 		 (define-key company-active-map (kbd "TAB") 'company-select-next)
 		 (define-key company-active-map [tab] 'company-select-next)
@@ -12,8 +10,5 @@
 
                  (global-set-key (kbd "C-;") 'company-complete)
                  (define-key company-active-map (kbd "C-;") 'company-complete-selection)
-                 (define-key company-active-map (kbd "<return>") 'newline)
-
-                 ;;(define-key company-active-map [return] nil)
-
-                 (add-to-list 'company-backends 'company-emoji)))
+                 (define-key company-active-map (kbd "<return>") 'newline)))
+(add-hook 'after-init-hook 'global-company-mode)
