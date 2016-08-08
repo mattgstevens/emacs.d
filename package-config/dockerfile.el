20adf480246371ad3c9c5aab5c296e5c6c5fd677
Martin Josefsson <hello@martinjosefsson.com>
2016-08-08 12:49:25 +0900

    Add Smartparens in Dockerfile mode

--- package-config/dockerfile.el
+++ package-config/dockerfile.el
@@ -1,3 +1,5 @@
 (use-package dockerfile-mode
   :ensure t
-  :mode ("Dockerfile$" . dockerfile-mode))
+  :mode ("Dockerfile$" . dockerfile-mode)
+  :config (progn
+            (add-hook 'dockerfile-mode-hook #'smartparens-mode)))
