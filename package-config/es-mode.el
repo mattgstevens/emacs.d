1770d144092f5a658aea9102d5f7e9c8a92612a5
Martin Josefsson <hello@martinjosefsson.com>
2017-01-08 10:21:42 +0100

    Defer load of es-mode until opening a .es buffer

--- package-config/es-mode.el
+++ package-config/es-mode.el
@@ -1,13 +1,12 @@
 (use-package es-mode
   :ensure t
+  :mode "\\.es$"
+  :bind ("s-{" . json-insert-object)
   :config (progn (add-to-list 'load-path "/path/to/es-mode-dir")
                  (autoload 'es-mode "es-mode.el"
                    "Major mode for editing Elasticsearch queries" t)
-                 (add-to-list 'auto-mode-alist '("\\.es$" . es-mode))
 
                  (add-hook 'es-mode-hook #'smartparens-mode)
                  (add-hook 'es-mode-hook #'aggressive-indent-mode)
-
-                 (add-hook 'es-mode-hook (global-set-key (kbd "s-{") #'json-insert-object))
 
                  (setq es-always-pretty-print t)))
