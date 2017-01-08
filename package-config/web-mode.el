8246405320c71e8c6a05ae50493e32a6da5a412c
Martin Josefsson <hello@martinjosefsson.com>
2017-01-08 12:51:45 +0100

    Defer load of web-mode until required

--- package-config/web-mode.el
+++ package-config/web-mode.el
@@ -1,11 +1,12 @@
 (use-package web-mode
   :ensure t
-  :config (progn
-            (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-            (setq web-mode-markup-indent-offset 2)
-            (setq web-mode-css-indent-offset 2)
-            (setq web-mode-code-indent-offset 2)
-            (setq web-mode-enable-auto-pairing t)
-            (setq web-mode-enable-current-column-highlight t)
+  :mode "\\.html?\\'"
+  :mode "\\.htm?\\'"
+  :init (progn
+          (setq web-mode-markup-indent-offset 2)
+          (setq web-mode-css-indent-offset 2)
+          (setq web-mode-code-indent-offset 2)
+          (setq web-mode-enable-auto-pairing t)
+          (setq web-mode-enable-current-column-highlight t)
 
-            (add-hook 'web-mode-hook #'aggressive-indent-mode)))
+          (add-hook 'web-mode-hook #'aggressive-indent-mode)))
