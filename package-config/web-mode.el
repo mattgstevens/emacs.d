6768afb13dc6527129350d703bd22d0a3ae8f2e4
Martin Josefsson <hello@martinjosefsson.com>
2017-02-28 17:23:37 +0900

    Make web-mode great for JS hacking

--- package-config/web-mode.el
+++ package-config/web-mode.el
@@ -1,7 +1,14 @@
 (use-package web-mode
   :ensure t
+  :ensure smartparens
+  :ensure rainbow-delimiters
+  :ensure aggressive-indent
+
   :mode "\\.html?\\'"
-  :mode "\\.htm?\\'"
+  :mode "\\html.erb\\'"
+  :mode "\\.jsx?"
+  :mode "\\.css\\'"
+  :mode "\\.scss\\'"
   :init (progn
           (setq web-mode-markup-indent-offset 2)
           (setq web-mode-css-indent-offset 2)
@@ -9,4 +16,6 @@
           (setq web-mode-enable-auto-pairing t)
           (setq web-mode-enable-current-column-highlight t)
 
-          (add-hook 'web-mode-hook #'aggressive-indent-mode)))
+          (add-hook 'web-mode-hook #'aggressive-indent-mode)
+          (add-hook 'web-mode-hook #'rainbow-delimiters-mode)
+          (add-hook 'web-mode-hook #'smartparens-mode)))
