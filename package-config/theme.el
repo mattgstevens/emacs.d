28b059580777930a5d3cf45567d1d7985a725291
Martin Josefsson <hello@martinjosefsson.com>
2017-04-19 15:52:10 +0900

    Change theme to Dracula, spooky!

--- package-config/theme.el
+++ package-config/theme.el
@@ -1,3 +1,11 @@
-(use-package darkokai-theme
+;; (use-package darkokai-theme
+;;   :ensure t
+;;   :config (load-theme 'darkokai t))
+
+(use-package dracula-theme
   :ensure t
-  :config (load-theme 'darkokai t))
+  :config (load-theme 'dracula t))
+
+;; (use-package atom-one-dark-theme
+;;   :ensure t
+;;   :config (load-theme 'atom-one-dark t))
