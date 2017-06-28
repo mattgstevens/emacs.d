cda7a6669f1b4319d54e5cd50860c2add022aa15
Martin Josefsson <hello@martinjosefsson.com>
2017-06-28 11:01:03 +0200

    Add inf-ruby to emacs config

--- package-config/ruby-mode.el
+++ package-config/ruby-mode.el
@@ -1,8 +1,13 @@
 (use-package ruby-mode
   :ensure t
+  :ensure inf-ruby
+  :ensure company
+  :ensure company-inf-ruby
   :mode "\\.rb\\'"
   :mode "Rakefile"
   :mode "Gemfile"
   :ensure aggressive-indent
   :config (progn
-            (add-hook 'ruby-mode-hook #'aggressive-indent-mode)))
+            (add-hook 'ruby-mode-hook #'aggressive-indent-mode)
+            (eval-after-load 'company
+              '(add-to-list 'company-backends 'company-inf-ruby))))
