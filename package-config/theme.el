a4912ae76a43bcbc4f6e55edfe6923b3b661dc9a
Martin Josefsson <m.josefsson@searchmetrics.com>
2017-09-08 11:17:35 +0200

    Add disable-all-themes procedure

--- package-config/theme.el
+++ package-config/theme.el
@@ -1,3 +1,11 @@
+(defun disable-all-themes ()
+  "disable all active themes."
+  (interactive)
+  (dolist (i custom-enabled-themes)
+    (disable-theme i)))
+
+(disable-all-themes)
+
 ;; (use-package darkokai-theme
 ;;   :ensure t
 ;;   :config (load-theme 'darkokai t))
