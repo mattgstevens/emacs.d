e59510c742a03b11a8f4f3a5f27834409ffda3af
Martin Josefsson <hello@martinjosefsson.com>
2017-10-28 09:56:06 +0200

    Aggressive indent, prettier et al for json-mode

--- package-config/json-mode.el
+++ package-config/json-mode.el
@@ -1,2 +1,9 @@
 (use-package json-mode
-  :ensure t)
+  :ensure t
+  :ensure smartparens
+  :ensure rainbow-delimiters
+  :ensure aggressive-indent
+  :config (progn
+            (add-hook 'json-mode-hook #'smartparens-mode)
+            (add-hook 'json-mode-hook #'rainbow-delimiters-mode)
+            (add-hook 'json-mode-hook #'aggressive-indent-mode)))
