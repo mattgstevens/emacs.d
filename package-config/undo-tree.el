307c54e070ae27f974a7f42a3b872a6b35d0028b
Martin Josefsson <hello@martinjosefsson.com>
2017-11-20 10:24:47 +0100

    Set s-Z (aka Shift-Cmd-Z on Darwin) to do `undo-tree-redo`

--- package-config/undo-tree.el
+++ package-config/undo-tree.el
@@ -1,4 +1,5 @@
 (use-package undo-tree
   :ensure t
   :config (progn
-            (global-undo-tree-mode)))
+            (global-undo-tree-mode)
+            (global-set-key (kbd "s-Z") 'undo-tree-redo)))
