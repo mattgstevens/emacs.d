568e803d224ece856efa0e8828f8498c6894f3b3
mattgstevens <mattgstevens@gmail.com>
2017-11-21 17:22:16 +0100

    update: web-mode can flow type

--- package-config/web-mode.el
+++ package-config/web-mode.el
@@ -2,24 +2,35 @@
   :ensure t
   :ensure smartparens
   :ensure rainbow-delimiters
-  :ensure aggressive-indent
+  ;; :ensure aggressive-indent
   :ensure prettier-js
+  :ensure flycheck-flow
 
   :mode "\\.html?\\'"
+  :mode "\\.json\\'"
   :mode "\\html.erb\\'"
   :mode "\\.jsx?"
   :mode "\\.css\\'"
   :mode "\\.scss\\'"
-  :init (progn
-          (setq web-mode-markup-indent-offset 2)
-          (setq web-mode-css-indent-offset 2)
-          (setq web-mode-code-indent-offset 2)
-          (setq web-mode-enable-auto-pairing t)
-          (setq web-mode-enable-current-column-highlight t)
+  :mode "\\.babelrc\\'"
+  :config (progn
 
-          (add-hook 'web-mode-hook #'aggressive-indent-mode)
-          (add-hook 'web-mode-hook #'rainbow-delimiters-mode)
-          (add-hook 'web-mode-hook #'smartparens-mode)
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            (setq web-mode-enable-auto-pairing t)
+            (setq web-mode-enable-current-column-highlight t)
 
-          (setq prettier-js-args '("--no-semi"))
-          (add-hook 'web-mode-hook 'prettier-js-mode)))
+            ;; (add-hook 'web-mode-hook #'aggressive-indent-mode)
+            (add-hook 'web-mode-hook #'rainbow-delimiters-mode)
+            (add-hook 'web-mode-hook #'smartparens-mode)
+
+            (setq prettier-js-args '("--no-semi"))
+            (add-hook 'web-mode-hook 'prettier-js-mode)
+
+            (add-hook 'web-mode-hook #'linum-mode)
+
+            (require 'flycheck-flow)
+            (add-hook 'web-mode-hook 'flycheck-mode)
+            (flycheck-add-mode 'javascript-flow 'web-mode)
+            (flycheck-select-checker 'javascript-flow)))
